157ea45df0a690e33fe97ddabcb3fb48ecd3dfd0
Jose Victor <ragjvictor@gmail.com>
2024-04-03 18:10:41 -0400

    Finishing attendee service

---
+++ src/main/java/rocketseat/com/passin/dto/attendee/AttendeeDetailDTO.java
@@ -0,0 +1,13 @@
+package rocketseat.com.passin.dto.attendee;
+
+import java.time.LocalDateTime;
+
+public record AttendeeDetailDTO(
+        String id,
+        String name,
+        String email,
+        LocalDateTime createdAt,
+        LocalDateTime checkedInAt
+){
+
+}
---
+++ src/main/java/rocketseat/com/passin/services/AttendeeService.java
@@ -0,0 +1,44 @@
+package rocketseat.com.passin.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import rocketseat.com.passin.domain.attendee.Attendee;
+import rocketseat.com.passin.domain.checkin.Checkin;
+import rocketseat.com.passin.dto.attendee.AttendeeDetailDTO;
+import rocketseat.com.passin.dto.attendee.AttendeesListResponseDTO;
+import rocketseat.com.passin.repositories.AttendeeRepository;
+import rocketseat.com.passin.repositories.CheckinRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class AttendeeService {
+
+    private AttendeeRepository attendeeRepository;
+
+    private CheckinRepository checkinRepository;
+
+    public List<Attendee> getAllAttendeesFromEvent(String eventId){
+        return this.attendeeRepository.findByEventId(eventId);
+    }
+
+    public AttendeesListResponseDTO getEventsAttendee(String eventId){
+        List<Attendee> attendeeList = this.getAllAttendeesFromEvent(eventId);
+
+        List<AttendeeDetailDTO> attendeeDetailDTOList = attendeeList.stream().map(attendee -> {
+            Optional<Checkin> checkin = this.checkinRepository.findByAttendeeId(attendee.getId());
+            LocalDateTime checkedInAt = checkin.<LocalDateTime>map(Checkin::getCreatedAt).orElse(null);
+            return new AttendeeDetailDTO(
+                    attendee.getId(),
+                    attendee.getName(),
+                    attendee.getEmail(),
+                    attendee.getCreatedAt(),
+                    checkedInAt);
+        }).toList();
+
+        return new AttendeesListResponseDTO(attendeeDetailDTOList);
+    }
+}
